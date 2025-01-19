88042059114002edf6576d14d145c7eecad4fc3b
busraygul <aygul.busra58@gmail.com>
2025-01-19 14:16:54 +0300

    01-request

---
+++ Dersler/Ders05-Http-Async/01-Request.js
@@ -0,0 +1,19 @@
+document.getElementById('veri').addEventListener('click', function () {
+    // XMLHttpRequest nesnesi oluşturuluyor.
+    const xhr = new XMLHttpRequest();
+    console.log(xhr);
+    xhr.onreadystatechange = function () {
+        console.log(xhr.readyState);
+        if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(xhr.responseText);
+    //document.write(xhr.responseText);
+}
+if (xhr.readyState === 4 && xhr.status === 404) {
+    console.log('Veri bulunamadı');
+}
+};
+// xhr.open('GET',"texxt.txt",true); hata alabilmek için yanlış yazdım.
+
+xhr.open('GET',"text.txt",true);
+xhr.send();
+});
